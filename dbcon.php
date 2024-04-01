1c6d746350781e3834b5d03b1e2a27482b51917f
shibjyoti555 <74619856+shibjyoti555@users.noreply.github.com>
2024-04-02 01:02:44 +0530

    Add files via upload

--- dbcon.php
+++ dbcon.php
@@ -2,10 +2,10 @@
 
 $host = $_SERVER['HTTP_HOST'];
 if ($host == "railsoft.in") {
-    $conn = mysqli_connect("localhost", "rail", "F#tyv2h@5h6V", "rail_user");
+    // $conn = mysqli_connect("localhost", "rail", "F#tyv2h@5h6V", "rail_user");
 }
 elseif($host == "suryacoaching.com"){
-    $conn = mysqli_connect("localhost", "suryac", "Qq{nLW%y*0gG", "rail_user");
+    // $conn = mysqli_connect("localhost", "suryac", "Qq{nLW%y*0gG", "rail_user");
 }
 else {
     // $conn = mysqli_connect("localhost", "root", "", "nice");
